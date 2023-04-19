2436393004e24a63d9a2a41dacbee9164ae3636a
omr-web <gtuomer@gmail.com>
2023-04-19 22:19:13 +0300

    first step

---
+++ build/CMakeFiles/Image.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/omer/omr-web/Image-Processing/src/image_lib.cpp" "CMakeFiles/Image.dir/src/image_lib.cpp.o" "gcc" "CMakeFiles/Image.dir/src/image_lib.cpp.o.d"
+  "/home/omer/omr-web/Image-Processing/src/main.cpp" "CMakeFiles/Image.dir/src/main.cpp.o" "gcc" "CMakeFiles/Image.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Image.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Image.dir/src/image_lib.cpp.o"
+  "CMakeFiles/Image.dir/src/image_lib.cpp.o.d"
+  "CMakeFiles/Image.dir/src/main.cpp.o"
+  "CMakeFiles/Image.dir/src/main.cpp.o.d"
+  "Image"
+  "Image.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Image.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
